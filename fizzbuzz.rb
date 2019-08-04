79c2f655a4fcbbffd432a66e892bc69395ceea08
Rida Kodad <kodadreda@gmail.com>
2019-08-04 13:46:46 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz(15)
+def fizzbuzz(number)
   if number % 3 && number % 5
     puts "FizzBuzz"
   elsif number % 3
