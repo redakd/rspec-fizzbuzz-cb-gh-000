1b6c8a8131c28dcb3ed5fd4f14aca2335ee204f5
Rida Kodad <kodadreda@gmail.com>
2019-08-04 13:49:15 +0000

    Automatically backed up by Learn

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -7,7 +7,7 @@ describe "fizzbuzz" do
   it 'returns "Fizz" when the number is divisible by 3' do
     fizz_3 = fizzbuzz(3)
 
-    expect(fizzbuzz(3)).to eq("Fizz")
+    expect(fizz_3).to eq("Fizz")
   end
   it 'returns "Buzz" when the number is divisible by 5' do
     fizz_5 = fizzbuzz(5)
